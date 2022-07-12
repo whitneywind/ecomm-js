3326020babdca854268a724a1eeaa2c9713ccf21
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-07-12 10:23:35 -0700

    admin panel finish

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,13 +5,13 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', requireAuth, async (req, res) => {
-  
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
@@ -34,5 +34,46 @@ router.post(
     res.redirect('/admin/products');
   }
 );
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+  const product = await productsRepo.getOne(req.params.id);
+  if (!product) {
+    return res.send('product not found');
+  }
+
+  res.send(productsEditTemplate({ product }));
+});
+
+router.post(
+  '/admin/products/:id/edit', 
+  requireAuth, 
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+
+      if (req.file) {
+        changes.image = req.file.buffer.toString('base64');
+      }
+
+    try {  
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('could not find item');
+    }
+
+    res.redirect('/admin/products');
+  }
+);
+
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id);
+
+  res.redirect('/admin/products');
+})
 
 module.exports = router;
